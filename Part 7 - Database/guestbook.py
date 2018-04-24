9ec4a017bff44423d2d4c97321fb6e9e363b30d3
Tutorial Doctor <thetutorialdoctor@gmail.com>
2018-04-23 20:38:57 -0400

    updated with sqlite database
    Modified guestbook.py and added an sqlite database.

--- Part 7 - Database/guestbook.py
+++ Part 7 - Database/guestbook.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://scott:tiger@localhost/mydatabase'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
